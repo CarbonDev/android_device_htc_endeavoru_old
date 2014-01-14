29c7740265bb9b0c32dba600017ce9d78f620a7f
Thomas Wendt <thoemy@gmx.net>
2014-01-15 00:56:45 +0100

    endeavoru: Remove duplicate vendor
    We don't want a HTC HTC One X.
    
    Change-Id: I9c97d1431e648de9d549f6bade7f6afe4b819ff6

--- carbon.mk
+++ carbon.mk
@@ -21,7 +21,7 @@ PRODUCT_LOCALES := en_US de_DE zh_CN zh_TW cs_CZ nl_BE nl_NL en_AU en_GB en_CA e
 PRODUCT_DEVICE := endeavoru
 PRODUCT_NAME := carbon_endeavoru
 PRODUCT_BRAND := htc
-PRODUCT_MODEL := HTC One X
+PRODUCT_MODEL := One X
 PRODUCT_MANUFACTURER := HTC
 
 #Set build fingerprint / ID / Product Name ect.
